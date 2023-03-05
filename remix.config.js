cda32fbac90e8d478cf1d85775f69a5c0502177b
PaynePew <42099584+PaynePew@users.noreply.github.com>
2023-03-05 16:54:40 +0800

    chore: update netlify.toml and remix config

--- remix.config.js
+++ remix.config.js
@@ -3,8 +3,8 @@ module.exports = {
     serverBuildTarget: "netlify",
     server: process.env.NETLIFY || process.env.NETLIFY_LOCAL ? "./server.js" : undefined,
     ignoredRouteFiles: ["**/.*"],
-    appDirectory: "app",
-    assetsBuildDirectory: "public/build",
+    // appDirectory: "app",
+    // assetsBuildDirectory: "public/build",
     // serverBuildPath: ".netlify/functions-internal/server.js",
-    publicPath: "/build/",
+    // publicPath: "/build/",
 };

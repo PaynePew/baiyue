6c27e262b309739d6d022a6175f5d34cb29362dc
PaynePew <nlpam06@gmail.com>
2024-04-17 20:20:21 +0800

    feat(error page): add error boundary and error page

--- app/root.tsx
+++ app/root.tsx
@@ -4,6 +4,7 @@ import { Header } from "~/components/Header";
 import tailWindStyles from "./styles/app.css";
 import headerStyles from "~/components/Header/styles.css";
 import footerStyles from "~/components/Footer/styles.css";
+import { NotFoundPage } from "./pages/ErrorPage/NotFound";
 
 export const meta: MetaFunction = () => ({
     charset: "utf-8",
@@ -23,6 +24,7 @@ export default function App() {
     return (
         <html lang="en">
             <head>
+                <title>百越工程</title>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link
@@ -35,6 +37,30 @@ export default function App() {
             <body className="bg-grayscale-iron">
                 <Header />
                 <Outlet />
+                <ScrollRestoration />
+                <Scripts />
+                <LiveReload />
+            </body>
+        </html>
+    );
+}
+export function ErrorBoundary() {
+    return (
+        <html lang="en">
+            <head>
+                <title>無法找到此頁面</title>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Gafata&family=Noto+Sans+TC:wght@100;400&display=swap"
+                    rel="stylesheet"
+                />
+                <Meta />
+                <Links />
+            </head>
+            <body className="bg-grayscale-iron">
+                <Header />
+                <NotFoundPage />
                 <ScrollRestoration />
                 <Scripts />
                 <LiveReload />

f0b4362cafceae3af0720e1ca141a7bb9eaebe57
PaynePew <nlpam06@gmail.com>
2024-12-11 15:35:02 +0800

    feat(projects category): add SEO to /projects/$categoryId

--- app/routes/projects/$categoryId.tsx
+++ app/routes/projects/$categoryId.tsx
@@ -2,7 +2,7 @@ import { useMemo } from "react";
 import { client } from "~/contentful.server";
 import { useLoaderData } from "react-router";
 import { ProjectPage } from "~/pages/ProjectPage";
-import type { LoaderArgs } from "@remix-run/node";
+import type { MetaFunction, LoaderArgs } from "@remix-run/node";
 export { links } from "~/pages/ProjectPage";
 export { ProjectPage } from "~/pages/ProjectPage";
 
@@ -15,6 +15,23 @@ const projectTab = [
     { title: "營區", tag: "military" },
     { title: "其他", tag: "others" },
 ];
+
+export const meta: MetaFunction = () => {
+    return {
+        charset: "utf-8",
+        viewport: "width=device-width,initial-scale=1",
+        title: "工程實績",
+        description: "BIM工程實際案例",
+        // OG Meta Tag
+        "og:title": "工程實績",
+        "og:description": "BIM工程實際案例",
+        "og:type": "website",
+        //Twitter Card Meta Tag
+        "twitter:card": "summary_large_image",
+        "twitter:title": "工程實績",
+        "twitter:description": "BIM工程實際案例",
+    };
+};
 
 export const loader = async ({ params }: LoaderArgs) => {
     const tag = params.categoryId;

edae2ccfc72227ecbcb96e74c890a8eb4047bbc9
PaynePew <nlpam06@gmail.com>
2024-12-12 23:18:22 +0800

    fix(partners): fix partnersData possibily null

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,5 @@
+import type { PartnerData } from "~/types/PartnerTypes";
 import { useMemo } from "react";
-import { json } from "@remix-run/node";
 import { useLoaderData } from "react-router";
 import { client } from "~/contentful.server";
 import { LandingPage } from "~/pages/LandingPage";
@@ -11,15 +11,16 @@ export const loader = async () => {
     if (!entries) {
         throw new Response("Not Found", { status: 404 });
     }
-    return json(entries.items);
+    return entries.items;
 };
 
 export default function LandingPageIndex() {
-    const partnersData = useLoaderData();
+    const partnersData = useLoaderData() as PartnerData[];
     const sortedPartnersData = useMemo(() => {
         if (Array.isArray(partnersData) && partnersData.length > 0) {
             return [...partnersData].sort((a, b) => b.fields.order - a.fields.order);
         }
+        return [];
     }, [partnersData]);
     return <LandingPage partnersData={sortedPartnersData} />;
 }
